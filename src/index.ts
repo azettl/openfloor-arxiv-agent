14be64f8c6c0ed0f28558a03fc54aa6e0f6ad29f
Andreas <info@azettl.net>
2025-07-24 20:47:57 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import app from './arxiv-server';
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`Parrot Agent server running on port ${PORT}`);
+  console.log(`Arxiv Agent server running on port ${PORT}`);
 });
